3716bef5f93ac2f6a26b24bbba001fde2fbe38d2
NikitaMalinovskii <95215330+NikitaMalinovskii@users.noreply.github.com>
2022-06-23 23:48:03 +0700

    Add files via upload

---
+++ components/certificates.php
@@ -0,0 +1,8 @@
+<img 
+     class="w-100 mt-4"
+     src="../assets/img/certificates/cert1.jpeg"
+     alt="Сертификат ООО 'НПП Микропроцессорные технологии'">
+<img 
+     class="w-100 mt-5"
+     src="../assets/img/certificates/cert2.png"
+     alt="Сертификат ООО 'Чинт Электрик'">
