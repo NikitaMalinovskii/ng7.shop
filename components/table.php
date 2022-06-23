ad598e231f5565fa1c5934f576c6975081e657c0
NikitaMalinovskii <95215330+NikitaMalinovskii@users.noreply.github.com>
2022-06-23 23:47:49 +0700

    Create table.php

---
+++ components/table.php
@@ -0,0 +1,210 @@
+<table class="table table-bordered m-0" style="width: 100%">
+  <thead>
+    <tr>
+      <th colspan="3" scope="col" style="text-align: left;">Наименование</th>
+      <th scope="col">Единицы измерения</th>
+      <th scope="col">Функция выключателя нагрузки</th>
+      <th scope="col">Функция выключателя нагрузки с плавким предохранителем</th>
+      <th scope="col">Функция силового выключателя</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td colspan="3">Функции</td>
+      <td></td>
+      <td>C</td>
+      <td>F</td>
+      <td>V</td>
+    </tr>
+    <tr>
+      <td colspan="3">Наибольшее рабочее напряжение</td>
+      <td>кВ</td>
+      <td>12/24/40,5</td>
+      <td>12/24/40,5</td>
+      <td>12/24/40,5</td>
+    </tr>
+    <tr>
+      <td colspan="3">Номинальное напряжение</td>
+      <td>кВ</td>
+      <td>10/20/35</td>
+      <td>10/20/35</td>
+      <td>10/20/35</td>
+    </tr>
+    <tr>
+      <td colspan="3">Номинальная частота</td>
+      <td>Гц</td>
+      <td>50/60</td>
+      <td>50/60</td>
+      <td>50/60</td>
+    </tr>
+    <tr>
+      <td colspan="3">Номинальный ток</td>
+      <td>A</td>
+      <td>630</td>
+      <td>
+        < 125 (12 кВ) <br>
+        < 100 (24 кВ) <br>
+        < 63 (40,5 кВ) <br>
+      </td>
+      <td>630</td>
+    </tr>
+    <tr>
+      <td rowspan="5">Уровень изоляции</td>
+      <td rowspan="2" style="text-align: left;">Выдерживаемое
+        напряжение
+        промышленной
+        частоты</td>
+      <td style="text-align: left;">Выдерживаемое напряжение
+        промышленной частоты (фазное)</td>
+      <td>кВ</td>
+      <td>42/65/95</td>
+      <td>42/65/95</td>
+      <td>42/65/95</td>
+    </tr>
+    <tr>
+      <td>Выдерживаемое напряжение
+        промышленной частоты
+        (линейное)</td>
+      <td></td>
+      <td>48/79/118</td>
+      <td>
+        48/79/118
+      </td>
+      <td>48/79/118</td>
+    </tr>
+    <tr>
+      <td rowspan="3" style="text-align: left;">Импульсное
+        выдерживаемое
+        напряжение </td>
+      
+          <td style="text-align: left;">Выдерживаемое
+            напряжение промышленной
+            частоты (контрольные и
+            вспомогательные цепи)</td>
+          <td></td>
+          <td>2/2/2 </td>
+          <td>
+            2/2/2 
+          </td>
+          <td>2/2/2 </td>
+        
+    </tr>
+    <tr>
+      <td>Выдерживаемое импульсное
+        напряжение (фазное) </td>
+      <td></td>
+      <td>75/125/185</td>
+      <td>
+        75/125/185
+      </td>
+      <td>75/125/185</td>
+    </tr>
+    <tr>
+      <td>Выдерживаемое импульсное
+        напряжение (линейное)</td>
+      <td></td>
+      <td>85/145/215</td>
+      <td>
+        85/145/215
+      </td>
+      <td>85/145/215</td>
+    </tr>
+    <tr>
+      <td rowspan="3" colspan="2">Ток термической стойкости</td>
+      <td style="text-align: left;">Главные цепи/4с</td>
+      <td>кA</td>
+      <td>20/20</td>
+      <td>—</td>
+      <td>20/25</td>
+    </tr>
+    <tr>
+      <td style="text-align: left;">Цепи заземления/4с</td>
+      <td></td>
+      <td>20/20</td>
+      <td>—</td>
+      <td>20/25</td>
+    </tr>
+    <tr>
+      <td style="text-align: left;">Цепи подключения
+        заземления/4с</td>
+      <td></td>
+      <td>17,4/17,4</td>
+      <td>—</td>
+      <td>17,4/17,4</td>
+    </tr>
+    <tr>
+      <td colspan="3" style="text-align: left;">Номинальный ток включения короткого замыкания</td>
+      <td>кA</td>
+      <td>50</td>
+      <td>80</td>
+      <td>50/63</td>
+    </tr>
+    <tr>
+      <td colspan="3" style="text-align: left;">Номинальный ток отключения при коротком замыкании</td>
+      <td>кA</td>
+      <td>—</td>
+      <td>31,5</td>
+      <td>20/25</td>
+    </tr>
+    <tr>
+      <td colspan="3" style="text-align: left;">Номинальный ток отключения активной нагрузки</td>
+      <td>A</td>
+      <td>630</td>
+      <td>—</td>
+      <td>—</td>
+    </tr>
+    <tr>
+      <td rowspan="2" colspan="2">Механический ресурс</td>
+      <td style="text-align: left;">Выключатель нагрузки/
+        выключатель</td>
+      <td></td>
+      <td>5000</td>
+      <td>5000</td>
+      <td>10000</td>
+    </tr>
+    <tr>
+      <td style="text-align: left;">Разъединители/Заземлитель</td>
+      <td></td>
+      <td>5000</td>
+      <td>5000</td>
+      <td>5000</td>0
+    </tr>
+    <tr>
+      <td rowspan="2" colspan="2">Номинальное давление
+        элегаза SF6</td>
+      <td rowspan="2" style="text-align: left;">Выключатель нагрузки/
+        выключатель</td>
+      <td rowspan="2">МПа</td>
+      <td>0,04</td>
+      <td>0,04</td>
+      <td>0,04</td>
+    </tr>
+    <tr>
+      <td style="text-align: center;">0,02</td>
+      <td>0,02</td>
+      <td>0,02</td>
+    </tr>
+    <tr>
+      <td rowspan="2" colspan="2">Степень защиты</td>
+      <td style="text-align: left;">Отсек первичных цепей</td>
+      <td></td>
+      <td>IP67</td>
+      <td>IP67</td>
+      <td>IP67</td>
+    </tr>
+    <tr>
+      <td style="text-align: left;">Низковольтный отсек</td>
+      <td></td>
+      <td style="text-align: center;">IP4х</td>
+      <td>IP4х</td>
+      <td>IP4х</td>
+    </tr>
+    <tr>
+      <td colspan="3" style="text-align: left;">Максимальная ежегодная утечка элегаза, не более </td>
+      <td>%/год</td>
+      <td>< 0,01</td>
+      <td>< 0,01</td>
+      <td>< 0,01</td>
+    </tr>
+  </tbody>
+</table>
